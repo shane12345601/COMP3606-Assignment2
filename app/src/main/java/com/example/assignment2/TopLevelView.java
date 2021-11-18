876ff278703cd1194fafcb518a431ae5de618b82
shane12345601 <shane12345601@gmail.com>
2021-11-18 00:05:28 -0400

    Top View added

--- app/src/main/java/com/example/assignment2/TopLevelView.java
+++ app/src/main/java/com/example/assignment2/TopLevelView.java
@@ -3,6 +3,9 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class TopLevelView extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class TopLevelView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toplevelview);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.topViewOptions));
+        listView.setAdapter(listAdapter);
     }
 }
