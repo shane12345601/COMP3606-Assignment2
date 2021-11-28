9b024f5d0dbe9ed9b3d59f83962852d36947fa1d
cumberson97 <60389903+cumberson97@users.noreply.github.com>
2021-11-28 13:48:45 -0400

    database

--- app/src/main/java/com/example/assignment2/TopLevelView.java
+++ app/src/main/java/com/example/assignment2/TopLevelView.java
@@ -2,13 +2,19 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class TopLevelView extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toplevelview);
     }
+
+
 }
